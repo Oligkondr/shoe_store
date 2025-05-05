d31a4ad65f5066db6f7e98e99d832d174a22b4c5
Oleg <oligkondr@mail.ru>
2025-05-05 14:04:30 +0300

    Commit

--- app/routers/admins.py
+++ app/routers/admins.py
@@ -1,15 +1,17 @@
 from types import new_class
 
 from fastapi import APIRouter, HTTPException, status, Depends
+from h11 import PRODUCT_ID
 from sqlalchemy import create_engine, select
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, selectinload, subqueryload, joinedload
 
 from app.auth.auth_handler import create_access_token, verify_password, get_current_admin
 from app.database import session_maker
 from app.requests import AdminCreateRequest, AdminAuthRequest
 
-from app.models import Admin
+from app.models import Admin, Product, ModelColor, Color, Model
 from app.config import get_db_url
+from app.responses.responses import AdminResponse
 
 admins_router = APIRouter(prefix="/api/v1/admin", tags=["admin"])
 
@@ -57,7 +59,7 @@ def create_admin(admin: AdminCreateRequest):
     return {'success': True}
 
 
-@admins_router.post("/login", summary='Login admin')
+@admins_router.post("/login", summary='Login admin', response_model=AdminResponse)
 def login_admin(admin: AdminAuthRequest):
     with session_maker() as session:
         stmt = select(Admin).where(Admin.email == admin.email)
@@ -74,3 +76,140 @@ def login_admin(admin: AdminAuthRequest):
     })
 
     return {'access_token': access_token}
+
+
+@admins_router.get('/products', summary='Get all products', responses={
+    200: {
+        "description": "Удачный ответ",
+        "content": {
+            "application/json": {
+                "example":
+                    [
+                        {
+                            "model_color_id": 1,
+                            "quantity": 5000,
+                            "created_at": "2025-05-03T09:24:38.461000+00:00",
+                            "size_id": 1,
+                            "price": 5490,
+                            "id": 1,
+                            "updated_at": "2025-05-03T09:24:39.278000+00:00",
+                            "model_color": {
+                                "name": "bloody black",
+                                "id": 1,
+                                "updated_at": "2025-05-03T09:21:26.522000+00:00",
+                                "model_id": 1,
+                                "color_id": 1,
+                                "created_at": "2025-05-03T09:21:24.919000+00:00",
+                                "model": {
+                                    "name": "super_sneaker",
+                                    "category_id": 1,
+                                    "created_at": "2025-05-03T09:17:49.297000+00:00",
+                                    "description": "best shoes",
+                                    "sex_id": 2,
+                                    "id": 1,
+                                    "updated_at": "2025-05-03T09:17:50.654000+00:00",
+                                    "category": {
+                                        "name": "sneakers",
+                                        "updated_at": "2025-05-03T09:15:40.093000+00:00",
+                                        "id": 1,
+                                        "created_at": "2025-05-03T09:15:38.225000+00:00"
+                                    }
+                                },
+                                "color": {
+                                    "created_at": "2025-05-03T09:14:25.981000+00:00",
+                                    "id": 1,
+                                    "name": "black, white, red",
+                                    "updated_at": "2025-05-03T09:14:27.501000+00:00",
+                                    "base_colors": [
+                                        {
+                                            "id": 1,
+                                            "hex": "000000",
+                                            "updated_at": "2025-05-03T09:12:30.148000+00:00",
+                                            "created_at": "2025-05-03T09:12:28.622000+00:00",
+                                            "name": "black"
+                                        },
+                                        {
+                                            "id": 2,
+                                            "hex": "ffffff",
+                                            "updated_at": "2025-05-03T09:12:48.892000+00:00",
+                                            "created_at": "2025-05-03T09:12:46.043000+00:00",
+                                            "name": "white"
+                                        },
+                                        {
+                                            "id": 3,
+                                            "hex": "ff3333",
+                                            "updated_at": "2025-05-03T09:23:13.681000+00:00",
+                                            "created_at": "2025-05-03T09:23:12.290000+00:00",
+                                            "name": "red"
+                                        }
+                                    ]
+                                }
+                            },
+                            "size": {
+                                "cm": "28",
+                                "id": 1,
+                                "updated_at": "2025-05-03T09:11:37.359000+00:00",
+                                "created_at": "2025-05-03T09:11:35.912000+00:00",
+                                "ru": "43"
+                            }
+                        }
+                    ]
+            }
+        }
+    }
+
+})
+def get_all_products():
+    with (session_maker() as session):
+        smtm = select(Product).options(
+            joinedload(Product.model_color).subqueryload(ModelColor.color).subqueryload(Color.base_colors),
+            joinedload(Product.model_color).subqueryload(ModelColor.model).subqueryload(Model.category),
+            joinedload(Product.size),
+        )
+        result = session.execute(smtm).unique().scalars().all()
+    return result
+
+
+@admins_router.post('/products', summary='Create new product')
+def get_all_products():
+    return {'message': 'Create new product'}
+
+
+@admins_router.post(f'/products/{id}', summary='Change product')
+def get_all_products():
+    return {'message': 'Change product'}
+
+
+@admins_router.delete(f'/products/{id}', summary='Delete product')
+def get_all_products():
+    return {'message': 'Delete product'}
+
+
+@admins_router.post(f'/colors', summary='Create new color')
+def get_all_products():
+    return {'message': 'Create new color'}
+
+
+@admins_router.delete(f'/colors/{id}', summary='Delete color')
+def delete_color():
+    return {'message': 'Delete color'}
+
+
+@admins_router.get(f'/orders', summary='Get all orders')
+def get_all_orders():
+    return {'message': 'Get all orders'}
+
+
+@admins_router.post(f'/orders/{id}', summary='Change order status')
+def change_order_status():
+    return {'message': 'Change order status'}
+
+
+@admins_router.get(f'/profile', summary='Get profile info')
+def get_profile_info():
+    return {'message': 'Get profile info'}
+
+
+@admins_router.post(f'/profile/{id}', summary='Change profile info')
+def change_profile_info():
+    return {'message': 'Change profile info'}
